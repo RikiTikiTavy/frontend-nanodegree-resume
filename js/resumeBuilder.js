d8e61eb73855d4901b5397d02e3af47db69958a1
Morra <theredballpoint.com>
2016-09-08 01:54:55 +0400

    Added a JavaScript resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,38 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+var work = {
+	"jobs": [
+
+{"employer": "AnnMarie",
+"title":"maintenance",
+"location":"LakeGeorge",
+"dates":"2014 of July",
+"description":"doing some painting"},
+
+{"employer": "Rocket Coorporation",
+"title":"Engineer",
+"location":"Korolev, Moscow state",
+"dates":"2016 of November",
+"description":"Rocket technology"}
+	]
+}
+
+
+var education = {
+	"schools":[
+	{
+		"name": "5-th School",
+	"city": "Yaransk",
+	"major": ["Mathematical", "English"]
+	},
+	{
+	"name": "University",
+	"city": "Kazan",
+	"major": ["Chemistry", "English"]
+	}
+	]
+}
+
